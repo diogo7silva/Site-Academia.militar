7b3d0a6ef2d5fdd5db0076ba4b36d60c65dc5f4b
Terror_Silva <diogofssilva7@gmail.com>
2019-05-16 16:47:52 +0100

    index

--- app.py
+++ app.py
@@ -5,5 +5,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/registo')
+def registo():
+    return render_template('registo.html')
+
+@app.route('/carrinho')
+def carrinho():
+    return render_template('carrinho.html')
+
 if __name__ == '__main__':
     app.run()
