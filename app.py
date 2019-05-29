ed51a95c6a0a442c9b94728e52c2fbd9fe60be27
Terror_Silva <diogofssilva7@gmail.com>
2019-05-29 11:41:42 +0100

    Acabar o rodapé e os icones

--- app.py
+++ app.py
@@ -22,6 +22,18 @@ def root():
 def mensagem():
     return render_template('mensagem.html')
 
+@app.route('/missão')
+def missão():
+    return render_template('missão.html')
+
+@app.route('/visão')
+def visão():
+    return render_template('visão.html')
+
+@app.route('/valores')
+def valores():
+    return render_template('valores.html')
+
 @app.route('/sobre')
 def sobre():
     return render_template('sobre.html')
@@ -39,6 +51,5 @@ def carrinho():
     return render_template('carrinho.html')
 
 
-
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
